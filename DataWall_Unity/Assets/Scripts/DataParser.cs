8f801991a3b5e19d91f2dfbf32525e6dc72d5359
ChaseMoudry <chasemoudry@gmail.com>
2017-12-07 16:55:20 -0700

    New: Informational debugs throughout the PARSE process (with timestamps). Change: "string[] result" is now a local variable instead of a field.  > This is to properly run the following line: "yield return new WaitUntil(() => result != null);". Change: Reverted WebGL memory back to 256 from 512

--- DataWall_Unity/Assets/Scripts/DataParser.cs
+++ DataWall_Unity/Assets/Scripts/DataParser.cs
@@ -8,15 +8,13 @@ public class DataParser : MonoBehaviour
 
     public static DataParser Parser;
 
-    [SerializeField]
-    private string[] result;
-
     private List<GameObject> categoryBoxList = new List<GameObject>();
 
     private void Awake()
     {
         Parser = this;
 
+        Parser.StopCoroutine("ParseFileData");
         Parser.StartCoroutine(Parser.ParseFileData(
             Data_SearchOptions.CurrentFilter_FormatType,
             Data_SearchOptions.CurrentFilter_ImmigrationType,
@@ -30,12 +28,14 @@ public class DataParser : MonoBehaviour
         ImmigrationType immigrationType = ImmigrationType.Unauthorized;
         DataYear dataYear = DataYear.Five;
 
+        StopCoroutine("ParseFileData");
         StartCoroutine(ParseFileData(formatType, immigrationType, dataYear));
     }
 
     public System.Collections.IEnumerator ParseFileData(FormatType formatType, ImmigrationType immigrationType, DataYear dataYear)
     {
-        Debug.Log("PARSE");
+        Debug.Log(Time.time + ": PARSE");
+        string[] result;
 
         //TODO: Data Persistence Validation (Eliminate parsing redundancy with query updates)
         //TODO: Multi-category support
@@ -50,48 +50,56 @@ public class DataParser : MonoBehaviour
                 .Combine(Application.streamingAssetsPath, formatType.ToString() + numberFormat + immigrationType.ToString())
                 + ".csv";
 
+            Debug.Log(Time.time + ": Filepath = " + filePath);
+
             if (filePath.Contains("://"))
             {
                 var www = Networking.UnityWebRequest.Get(filePath);
                 yield return www.Send();
-                this.result = www.downloadHandler.text.Split('\n');
+                result = www.downloadHandler.text.Split('\n');
             }
             else
             {
                 try
                 {
-                    this.result = System.IO.File.ReadAllLines(filePath);
+                    result = System.IO.File.ReadAllLines(filePath);
                 }
                 catch (System.IO.FileNotFoundException)
                 {
-                    Debug.Log("File could not be found. FilePath = " + filePath);
+                    Debug.Log(Time.time + ": File could not be found. FilePath = " + filePath);
                     throw;
                 }
             }
 
-            yield return new WaitUntil(() => this.result != null);
+            yield return new WaitUntil(() => result != null);
+
+            Debug.Log(Time.time + ": Data Successfully Retrieved From " + filePath + ".");
 
             //TODO: Multi-year parsing
 
             //Result Data equals comma-separated string array - each element equals timeline data for each line (a state)
             // > Sample Element: "1,2222,3,4444,55,666,77,888888,9999"
 
-            for (int i = 0; i < this.result.Length; i++)
+            for (int i = 0; i < result.Length; i++)
             {
-                this.result[i] = this.result[i].Split(',')[(int)dataYear - (int)DataYear.Five];
+                result[i] = result[i].Split(',')[(int)dataYear - (int)DataYear.Five];
             }
 
-            this.UI_AddCategoryBox(numberFormat);
+            Debug.Log(Time.time + ": Adding UI Elements.");
+
+            this.UI_AddCategoryBox(ref result, numberFormat);
         }
 
         #endregion
+
+        Debug.Log(Time.time + ": Force Rebuilding UI Layouts.");
 
         UnityEngine.UI.LayoutRebuilder.ForceRebuildLayoutImmediate(this.GetComponent<RectTransform>());
 
         yield break;
     }
 
-    private void UI_AddCategoryBox(string numberFormat)
+    private void UI_AddCategoryBox(ref string[] result, string numberFormat)
     {
         var textBox = Instantiate(Resources.Load(categoryBox_AssetName, typeof(GameObject)), this.transform) as GameObject;
 
@@ -111,19 +119,19 @@ public class DataParser : MonoBehaviour
             switch ((NumberFormat)System.Enum.Parse(typeof(NumberFormat), numberFormat))
             {
                 case NumberFormat.Nominal:
-                    info.stateDataList[i].text = this.result[i];
+                    info.stateDataList[i].text = result[i];
                     break;
                 case NumberFormat.Percentage:
-                    info.stateDataList[i].text = this.result[i] + '%';
+                    info.stateDataList[i].text = result[i] + '%';
 
                     if (i < length - 1)
                     {
-                        Graph_BarHandler.GraphHandler.barList[i].UpdateBarSize(float.Parse(this.result[i],
+                        Graph_BarHandler.GraphHandler.barList[i].UpdateBarSize(float.Parse(result[i],
                             System.Globalization.CultureInfo.InvariantCulture.NumberFormat));
                     }
                     break;
                 default:
-                    Debug.LogError("An error occurred while updating the search filter dropdowns.");
+                    Debug.LogError(Time.time + ": An error occurred while updating the search filter dropdowns.");
                     break;
             }
         }
