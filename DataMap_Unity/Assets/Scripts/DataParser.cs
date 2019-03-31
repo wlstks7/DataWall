ce69e6499e648cb859ed3279418944deef8a4fd8
ChaseMoudry <chasemoudry@gmail.com>
2019-03-30 21:55:48 -0700

    Updated DataMap with new intro screens

--- DataMap_Unity/Assets/Scripts/DataParser.cs
+++ DataMap_Unity/Assets/Scripts/DataParser.cs
@@ -1,217 +1,287 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Networking = UnityEngine.Networking;
 using Enums;
 
 [RequireComponent(typeof(Camera))]
 public class DataParser : MonoBehaviour
 {
-	private const string categoryBox_AssetName = "Timeline Data Container";
+    private const string categoryBox_AssetName = "Timeline Data Container";
 
-	internal static DataParser Instance { get; set; }
+    internal static DataParser Instance { get; set; }
 
-	[Header("UI Windows")]
-	[SerializeField]
-	private GameObject m_MapWindow;
-	[SerializeField]
-	private GameObject m_DataWindow;
-	[SerializeField]
-	private GameObject m_ContentContainer;
+    [Header("UI Windows")]
+    [SerializeField]
+    private GameObject m_MapWindow;
+    [SerializeField]
+    private GameObject m_DataWindow;
+    [SerializeField]
+    private GameObject m_ContentContainer;
 
-	[Header("Camera Defaults")]
-	[SerializeField]
-	private float m_DefaultOrthoSize = 5f;
+    [Header("Intro Window")]
+    [SerializeField]
+    private Image m_TitleText;
+    [SerializeField]
+    private Image m_DescriptionText;
+    [SerializeField]
+    private float m_FadeRate = 5f;
 
-	[Header("Camera Movement Settings")]
-	[SerializeField]
-	private float m_OrthoZoomSpeed = 0.1f;
-	[SerializeField]
-	private float m_CameraMoveSpeed = 0.1f;
+    [Header("Camera Defaults")]
+    [SerializeField]
+    private float m_DefaultOrthoSize = 5f;
 
-	private List<GameObject> categoryBoxList = new List<GameObject>();
+    [Header("Camera Movement Settings")]
+    [SerializeField]
+    private float m_OrthoZoomSpeed = 0.1f;
+    [SerializeField]
+    private float m_CameraMoveSpeed = 0.1f;
 
-	// TODO: Move camera functions to separate script
-	private Camera m_Camera;
+    private List<GameObject> categoryBoxList = new List<GameObject>();
 
-	private void Awake()
-	{
-		Instance = this;
+    // TODO: Move camera functions to separate script
+    private Camera m_Camera;
 
-		Instance.m_Camera = Instance.GetComponent<Camera>();
+    private void Awake()
+    {
+        Instance = this;
 
-		Instance.Test_ResetCamera_Hard();
-		Instance.CameraToMapView();
-	}
+        Instance.m_Camera = Instance.GetComponent<Camera>();
 
-	public void ResetCamera_Smooth()
-	{
-		UpdateCamera(Vector3.zero, Instance.m_DefaultOrthoSize);
-		CameraPoint.EnableAllPoints();
-	}
+        Instance.Test_ResetCamera_Hard();
+        Instance.CameraToMapView();
+    }
 
-	[ContextMenu("Reset Camera Position")]
-	private void Test_ResetCamera_Hard()
-	{
-		this.transform.position = Vector3.zero;
-		this.GetComponent<Camera>().orthographicSize = this.m_DefaultOrthoSize;
-	}
+    private void Start()
+    {
+        StartCoroutine("FadeIntroWindow");
+    }
 
-	public void CameraToDataView()
-	{
-		if (FilterFlags.IsDirty)
-		{
-			ClearStoredData();
-		}
+    private Color tempColor;
+    private float alphaDiff;
+    private float targetAlpha;
 
-		Instance.m_DataWindow.SetActive(true);
-		Instance.m_MapWindow.SetActive(false);
-	}
+    private System.Collections.IEnumerator FadeIntroWindow()
+    {
+        this.m_TitleText.enabled = true;
+        this.m_DescriptionText.enabled = false;
 
-	public void CameraToMapView()
-	{
-		Instance.m_MapWindow.SetActive(true);
-		Instance.m_DataWindow.SetActive(false);
+        yield return new WaitForSecondsRealtime(3.0f);
 
-		Instance.ResetCamera_Smooth();
-	}
+        // Fade out title
+        targetAlpha = 0f;
+        while (true)
+        {
+            tempColor = this.m_TitleText.color;
+            alphaDiff = Mathf.Abs(tempColor.a - targetAlpha);
+            if (alphaDiff > 0.0001f)
+            {
+                tempColor.a = Mathf.Lerp(tempColor.a, targetAlpha, this.m_FadeRate * Time.deltaTime);
+                this.m_TitleText.color = tempColor;
+            }
+            else
+            {
+                break;
+            }
+            yield return null;
+        }
 
-	internal static void UpdateCamera(Vector3 newPosition, float newOrthoSize)
-	{
-		Instance.StopCoroutine("UpdateCameraPosition");
-		Instance.StartCoroutine("UpdateCameraPosition", newPosition);
+        Debug.Log("Phase2");
 
-		Instance.StopCoroutine("UpdateCameraOrthoSize");
-		Instance.StartCoroutine("UpdateCameraOrthoSize", newOrthoSize);
-	}
+        this.m_DescriptionText.enabled = true;
+        tempColor = this.m_DescriptionText.color;
+        tempColor.a = 0;
+        this.m_DescriptionText.color = tempColor;
 
-	private System.Collections.IEnumerator UpdateCameraPosition(Vector3 newPosition)
-	{
-		while (true)
-		{
-			if (Instance.m_Camera.transform.position == newPosition)
-			{
-				yield break;
-			}
+        // Fade in description
+        targetAlpha = 1f;
+        while (true)
+        {
+            tempColor = this.m_DescriptionText.color;
+            alphaDiff = Mathf.Abs(tempColor.a - targetAlpha);
+            if (alphaDiff > 0.0001f)
+            {
+                tempColor.a = Mathf.Lerp(tempColor.a, targetAlpha, this.m_FadeRate * Time.deltaTime);
+                this.m_DescriptionText.color = tempColor;
+            }
+            else
+            {
+                break;
+            }
+            yield return null;
+        }
+        Debug.Log("Exiting Coroutine");
+    }
 
-			Instance.m_Camera.transform.position = Vector3
-				.Lerp(Instance.m_Camera.transform.position, newPosition, Instance.m_OrthoZoomSpeed);
+    public void ResetCamera_Smooth()
+    {
+        UpdateCamera(Vector3.zero, Instance.m_DefaultOrthoSize);
+        CameraPoint.EnableAllPoints();
+    }
 
-			yield return null;
-		}
-	}
+    [ContextMenu("Reset Camera Position")]
+    private void Test_ResetCamera_Hard()
+    {
+        this.transform.position = Vector3.zero;
+        this.GetComponent<Camera>().orthographicSize = this.m_DefaultOrthoSize;
+    }
 
-	private System.Collections.IEnumerator UpdateCameraOrthoSize(float newOrthoSize)
-	{
-		while (true)
-		{
-			if (Instance.m_Camera.orthographicSize == newOrthoSize)
-			{
-				yield break;
-			}
+    public void CameraToDataView()
+    {
+        if (FilterFlags.IsDirty)
+        {
+            ClearStoredData();
+        }
 
-			Instance.m_Camera.orthographicSize = Mathf
-				.Lerp(Instance.m_Camera.orthographicSize, newOrthoSize, Instance.m_CameraMoveSpeed);
+        Instance.m_DataWindow.SetActive(true);
+        Instance.m_MapWindow.SetActive(false);
+    }
 
-			yield return null;
-		}
-	}
+    public void CameraToMapView()
+    {
+        Instance.m_MapWindow.SetActive(true);
+        Instance.m_DataWindow.SetActive(false);
 
-	public void ParseData()
-	{
-		Instance.StopCoroutine("UpdateData");
-		Instance.StartCoroutine("UpdateData");
-	}
+        Instance.ResetCamera_Smooth();
+    }
 
-	private System.Collections.IEnumerator UpdateData()
-	{
-		Debug.Log(Time.time + ": PARSE START");
+    internal static void UpdateCamera(Vector3 newPosition, float newOrthoSize)
+    {
+        Instance.StopCoroutine("UpdateCameraPosition");
+        Instance.StartCoroutine("UpdateCameraPosition", newPosition);
 
-		string[] result;
+        Instance.StopCoroutine("UpdateCameraOrthoSize");
+        Instance.StartCoroutine("UpdateCameraOrthoSize", newOrthoSize);
+    }
 
-		//TODO: Multi-category support
+    private System.Collections.IEnumerator UpdateCameraPosition(Vector3 newPosition)
+    {
+        while (true)
+        {
+            if (Instance.m_Camera.transform.position == newPosition)
+            {
+                yield break;
+            }
 
-		FilterFlags.ApplyFilters();
+            Instance.m_Camera.transform.position = Vector3
+                .Lerp(Instance.m_Camera.transform.position, newPosition, Instance.m_OrthoZoomSpeed);
 
-		ClearStoredData();
+            yield return null;
+        }
+    }
 
-		foreach (string numberFormat in Enum.GetNames(typeof(NumberFormat)))
-		{
-			string filePath = System.IO.Path.Combine(
-				"https://raw.githubusercontent.com/RGRoland/DataWall/gh-pages/Files/DataWall-Files/DataMap/",
-				FilterFlags.CurrentFilters.Filter_Format.ToString() +
-				numberFormat +
-				FilterFlags.CurrentFilters.Filter_Legality.ToString() +
-				".csv");
+    private System.Collections.IEnumerator UpdateCameraOrthoSize(float newOrthoSize)
+    {
+        while (true)
+        {
+            if (Instance.m_Camera.orthographicSize == newOrthoSize)
+            {
+                yield break;
+            }
 
-			Debug.Log("Filepath: " + filePath);
+            Instance.m_Camera.orthographicSize = Mathf
+                .Lerp(Instance.m_Camera.orthographicSize, newOrthoSize, Instance.m_CameraMoveSpeed);
 
-			var www = Networking.UnityWebRequest.Get(filePath);
+            yield return null;
+        }
+    }
 
-			yield return www.SendWebRequest();
+    public void ParseData()
+    {
+        Instance.StopCoroutine("UpdateData");
+        Instance.StartCoroutine("UpdateData");
+    }
 
-			result = www.downloadHandler.text.Split('\n');
+    private System.Collections.IEnumerator UpdateData()
+    {
+        Debug.Log(Time.time + ": PARSE START");
 
-			Debug.Log(Time.time + ": " + numberFormat + " data successfully retrieved.");
-			Debug.Log(Time.time + ": Result length = " + result.Length);
+        string[] result;
 
-			//TODO: Multi-year parsing
+        //TODO: Multi-category support
 
-			// Result now equals comma-separated string array - each element equals timeline data for each line (a state)
-			// > Sample Element: "1,2222,3,4444,55,666,77,888888,9999"
+        FilterFlags.ApplyFilters();
 
-			result = result[FilterFlags.CurrentFilters.Filter_State.ToIndex()].Split(',');
+        ClearStoredData();
 
-			Debug.Log(Time.time + ": Adding UI Elements.");
+        foreach (string numberFormat in Enum.GetNames(typeof(NumberFormat)))
+        {
+            string filePath = System.IO.Path.Combine(
+                "https://raw.githubusercontent.com/RGRoland/DataWall/gh-pages/Files/DataWall-Files/DataMap/",
+                FilterFlags.CurrentFilters.Filter_Format.ToString() +
+                numberFormat +
+                FilterFlags.CurrentFilters.Filter_Legality.ToString() +
+                ".csv");
 
-			UI_AddCategoryBox(result, numberFormat);
+            Debug.Log("Filepath: " + filePath);
 
-			yield return null;
-		}
+            var www = Networking.UnityWebRequest.Get(filePath);
 
-		Debug.Log(Time.time + ": PARSE END");
+            yield return www.SendWebRequest();
 
-		yield break;
-	}
+            result = www.downloadHandler.text.Split('\n');
 
-	private static void UI_AddCategoryBox(string[] result, string numberFormat)
-	{
-		StateDataPanel boxInfo = Instantiate(Resources.Load<GameObject>(categoryBox_AssetName), Instance.m_ContentContainer.transform)
-			.GetComponent<StateDataPanel>();
+            Debug.Log(Time.time + ": " + numberFormat + " data successfully retrieved.");
+            Debug.Log(Time.time + ": Result length = " + result.Length);
 
-		Instance.categoryBoxList.Add(boxInfo.gameObject);
+            //TODO: Multi-year parsing
 
-		boxInfo.categoryLabel.text = FilterFlags.CurrentFilters.Filter_Format.ToString() + ": " + numberFormat;
+            // Result now equals comma-separated string array - each element equals timeline data for each line (a state)
+            // > Sample Element: "1,2222,3,4444,55,666,77,888888,9999"
 
-		int length = Enum.GetNames(typeof(DataYear)).Length;
+            result = result[FilterFlags.CurrentFilters.Filter_State.ToIndex()].Split(',');
 
-		for (int i = 0; i < length; i++)
-		{
-			switch ((NumberFormat)Enum.Parse(typeof(NumberFormat), numberFormat))
-			{
-				case NumberFormat.Nominal:
-					boxInfo.stateDataList[i].text = String.Format("{0:n0}", Int32.Parse(result[i]));
-					break;
-				case NumberFormat.Percentage:
-					boxInfo.stateDataList[i].text = result[i] + '%';
-					break;
-				default:
-					Debug.LogError(Time.time + ": An error occurred while updating the search filter dropdowns.");
-					break;
-			}
-		}
-	}
+            Debug.Log(Time.time + ": Adding UI Elements.");
 
-	private static void ClearStoredData()
-	{
-		foreach (GameObject box in Instance.categoryBoxList)
-		{
-			if (box != null)
-			{
-				Destroy(box);
-			}
-		}
+            UI_AddCategoryBox(result, numberFormat);
 
-		Instance.categoryBoxList.Clear();
-	}
+            yield return null;
+        }
+
+        Debug.Log(Time.time + ": PARSE END");
+
+        yield break;
+    }
+
+    private static void UI_AddCategoryBox(string[] result, string numberFormat)
+    {
+        StateDataPanel boxInfo = Instantiate(Resources.Load<GameObject>(categoryBox_AssetName), Instance.m_ContentContainer.transform)
+            .GetComponent<StateDataPanel>();
+
+        Instance.categoryBoxList.Add(boxInfo.gameObject);
+
+        boxInfo.categoryLabel.text = FilterFlags.CurrentFilters.Filter_Format.ToString() + ": " + numberFormat;
+
+        int length = Enum.GetNames(typeof(DataYear)).Length;
+
+        for (int i = 0; i < length; i++)
+        {
+            switch ((NumberFormat)Enum.Parse(typeof(NumberFormat), numberFormat))
+            {
+                case NumberFormat.Nominal:
+                    boxInfo.stateDataList[i].text = String.Format("{0:n0}", Int32.Parse(result[i]));
+                    break;
+                case NumberFormat.Percentage:
+                    boxInfo.stateDataList[i].text = result[i] + '%';
+                    break;
+                default:
+                    Debug.LogError(Time.time + ": An error occurred while updating the search filter dropdowns.");
+                    break;
+            }
+        }
+    }
+
+    private static void ClearStoredData()
+    {
+        foreach (GameObject box in Instance.categoryBoxList)
+        {
+            if (box != null)
+            {
+                Destroy(box);
+            }
+        }
+
+        Instance.categoryBoxList.Clear();
+    }
 }
